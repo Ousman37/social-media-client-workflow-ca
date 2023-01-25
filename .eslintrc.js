a2c55f863920103382476ca3db5f6188bd8efa17
Ethmane Didi <ousman.didibela@gmail.com>
2023-01-25 20:57:20 +0100

    Update eslint configuration to match new project

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    jest: true,
+    'cypress/globals': true,
+  },
+  rules: {
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+    'cypress/no-assigning-return-values': 'error',
+    'cypress/no-unnecessary-waiting': 'warn',
+    'cypress/assertion-before-screenshot': 'warn',
+    'cypress/no-force': 'warn',
+  },
+};
